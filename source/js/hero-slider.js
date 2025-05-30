6f903c48ef19515c19d36bbc05a04a59cc7130ac
Tatiana Senatorova <1975tanya@bk.ru>
2025-05-30 17:38:44 +0300

    fix: переделывает код снятия фокуса с кнопок неактивных слайдов

--- source/js/hero-slider.js
+++ source/js/hero-slider.js
@@ -27,38 +27,25 @@ export const heroSwiper = new Swiper('.swiper-hero', {
     clickable: true,
     renderBullet: function (index, bulletClass) {
       return `<button class=${bulletClass}><span class="visually-hidden">Перейти к слайду ${index + 1
-      }</span></button>`;
+        }</span></button>`;
     },
     enabled: true,
   },
 });
 
 
-const unfocusNonActiveSlides = () => {
-  slidesHero.forEach((slide, index) => {
-    if (index === heroSwiper.activeIndex) {
-      slide.querySelector('.hero-card__primary-button').removeAttribute('tabindex');
-    } else {
-      slide.querySelector('.hero-card__primary-button').setAttribute('tabindex', '-1');
-    }
-  });
+const unfocusNonActiveSlide = () => {
+  slidesHero[heroSwiper.activeIndex].querySelector('.hero-card__primary-button').removeAttribute('tabindex');
+  slidesHero[heroSwiper.previousIndex].querySelector('.hero-card__primary-button').setAttribute('tabindex', '-1');
 };
 
-// const changePaginationPosition = () => {
-//   if (document.documentElement.clientWidth < TABLET_WIDTH) {
-//     marginBottomPagination = slidesHeroInfoContent[heroSwiper.activeIndex].offsetHeight - HERO_MOBILE_WRAPPER_FOR_PAGINATION_HEIGHT + HERO_BOTTOM_SECTION_PADDING_MOBILE + HERO__PAGINATION_BOTTOM_PADDING;
-//     heroPagination.style.bottom = `${marginBottomPagination}px`;
-//     return;
-//     // marginBottomPagination = slidesHeroInfoContent[heroSwiper.activeIndex].offsetHeight + HERO_BOTTOM_SECTION_PADDING_TABLET;
-//     // heroPagination.style.bottom = `${marginBottomPagination}px`;
-//   }
-// }
-
-changePaginationPosition();
+const changePaginationPlace = () => {
+  console.log('123')
+}
 
 const onSliderSlideChange = () => {
-  unfocusNonActiveSlides();
-  // changePaginationPosition();
+  unfocusNonActiveSlide();
+  changePaginationPlace();
 };
 
 heroSwiper.on('slideChange', onSliderSlideChange);
