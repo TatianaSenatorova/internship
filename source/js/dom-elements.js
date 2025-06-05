4270fa5b6bae2447960a35dd18dd4a3802678270
Tatiana Senatorova <1975tanya@bk.ru>
2025-06-05 19:31:19 +0300

    feat: реализовано перемещение кнопок реальной навигации при смене слайда в hero

--- source/js/dom-elements.js
+++ source/js/dom-elements.js
@@ -6,8 +6,11 @@ export const buttonsSubMenu = document.querySelectorAll('.main-header__nav-butto
 export const subMenus = document.querySelectorAll('.main-header__sub-nav-list');
 export const sliderHero = document.querySelector('.hero__slider');
 export const slidesHero = sliderHero.querySelectorAll('.hero__slider-item');
-export const slidesHeroInfoContent = sliderHero.querySelectorAll('.hero-card__info');
+export const slidesHeroInfoContent = sliderHero.querySelectorAll('.hero-card__info-wrap');
 export const heroPagination = sliderHero.querySelector('.hero__slider-pagination');
+export const heroFalsePaginations = sliderHero.querySelectorAll('.hero-card__false-pagination');
+export const realPagination = sliderHero.querySelector('.hero__slider-pagination');
+
 export const modal = document.querySelector('.modal');
 export const buttonOpenModal = document.querySelector('.about__primary-button');
 export const buttonCloseModal = document.querySelector('.modal__close-button');
--- source/js/hero-slider.js
+++ source/js/hero-slider.js
@@ -2,11 +2,14 @@ import Swiper from 'swiper';
 import { Pagination } from 'swiper/modules';
 import {
   slidesHero,
+  sliderHero,
+  heroFalsePaginations,
+  realPagination,
   slidesHeroInfoContent,
   heroPagination
 } from './dom-elements.js';
 import {
-HTML_FOR_PAGINATION
+  PADDING_TOP_SLIDE_CONTENT
 } from './constants.js';
 
 export const heroSwiper = new Swiper('.swiper-hero', {
@@ -37,12 +40,33 @@ export const heroSwiper = new Swiper('.swiper-hero', {
 });
 
 
+// const setPaginationPosition = () => {
+//   console.log('123');
+// }
+
+const resizeObserver = new ResizeObserver((entries) => {
+  for (const entry of entries) {
+    if (entry.target.closest('.swiper-slide-active')) {
+      console.log(entry.borderBoxSize[0].blockSize);
+      setRealPaginationPosition();
+    }
+  }
+});
+
+const observeHeight = () => {
+  slidesHeroInfoContent.forEach((infoContent) => {
+    resizeObserver.observe(infoContent);
+  });
+};
+
+observeHeight();
+
 const unfocusNonActiveSlide = () => {
   slidesHero[heroSwiper.activeIndex].querySelector('.hero-card__primary-button').removeAttribute('tabindex');
   slidesHero[heroSwiper.previousIndex].querySelector('.hero-card__primary-button').setAttribute('tabindex', '-1');
 };
 
-// const changePaginationPlace = () => {
+// const changeRealPaginationPosition = () => {
 //     slidesHero[heroSwiper.previousIndex].querySelector('.hero-card__pagination-nebo-wrap').innerHTML = '';
 //     console.log(slidesHero[heroSwiper.previousIndex].querySelector('.hero-card__pagination-nebo-wrap'));
 //     slidesHero[heroSwiper.activeIndex].querySelector('.hero-card__pagination-nebo-wrap').innerHTML = HTML_FOR_PAGINATION;
@@ -51,7 +75,40 @@ const unfocusNonActiveSlide = () => {
 
 const onSliderSlideChange = () => {
   unfocusNonActiveSlide();
-  // changePaginationPlace();
+  observeHeight();
 };
 
-heroSwiper.on('slideChange', onSliderSlideChange);
+
+const renderFalseBullets = () => {
+  const numberOfSlides = heroSwiper.slides.length;
+  for (let index = 0; index < numberOfSlides; index++) {
+    for (let j = 0; j < numberOfSlides; j++) {
+      if (index === j) {
+        heroFalsePaginations[index].insertAdjacentHTML('beforeend', `<button class="pagination-bullet pagination-bullet--is-active"></button>`);
+      } else {
+        heroFalsePaginations[index].insertAdjacentHTML('beforeend', `<button class="pagination-bullet"></button>`);
+      }
+    }
+  }
+}
+
+const setRealPaginationPosition = () => {
+  const top = slidesHeroInfoContent[heroSwiper.activeIndex].offsetTop;
+  realPagination.style.top = `${top + PADDING_TOP_SLIDE_CONTENT}px`;
+  console.log(top, slidesHeroInfoContent[heroSwiper.activeIndex]);
+}
+
+renderFalseBullets();
+setRealPaginationPosition();
+
+const hideRealPagination = () => {
+  realPagination.style.display = 'none';
+}
+
+const showRealPagination = () => {
+  setRealPaginationPosition();
+  realPagination.style.display = 'flex';
+}
+
+heroSwiper.on('slideChange', hideRealPagination);
+heroSwiper.on('transitionEnd', showRealPagination);
